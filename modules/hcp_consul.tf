c069a343052713f7a0efc6fd458018d2338e66d1
Ricardo Oliveira <ricardo@hashicorp.com>
2021-07-09 14:20:53 +0100

    naming hcp resources with namespace prefix

--- modules/hcp_consul.tf
+++ modules/hcp_consul.tf
@@ -1,23 +1,23 @@
 
 resource "hcp_consul_cluster" "hcp_demostack" {
-  hvn_id     = hcp_hvn.demostack.hvn_id
-  cluster_id = var.hcp_consul_cluster_id
-  tier       = var.hcp_cluster_tier
-  datacenter = var.region
+  hvn_id          = hcp_hvn.demostack.hvn_id
+  cluster_id      = "${var.namespace}-consul"
+  tier            = var.hcp_cluster_tier
+  datacenter      = var.region
   public_endpoint = true
 }
 
 provider "consul" {
   address    = hcp_consul_cluster.hcp_demostack.consul_public_endpoint_url
-  datacenter =  hcp_consul_cluster.hcp_demostack.datacenter
-  token      =  hcp_consul_cluster.hcp_demostack.consul_root_token_secret_id
+  datacenter = hcp_consul_cluster.hcp_demostack.datacenter
+  token      = hcp_consul_cluster.hcp_demostack.consul_root_token_secret_id
 
 }
 
 resource "consul_acl_policy" "agent" {
-  name  = "agent"
+  name        = "agent"
   datacenters = [hcp_consul_cluster.hcp_demostack.datacenter]
-  rules = <<-RULE
+  rules       = <<-RULE
     node_prefix "" {
       policy = "write"
     }
@@ -25,32 +25,32 @@ resource "consul_acl_policy" "agent" {
 }
 
 resource "consul_acl_token" "agent-token" {
-  depends_on = [hcp_consul_cluster.hcp_demostack,hcp_vault_cluster.hcp_demostack]
-  count = var.workers
+  depends_on  = [hcp_consul_cluster.hcp_demostack, hcp_vault_cluster.hcp_demostack]
+  count       = var.workers
   description = "TF created agent token"
-  policies = [consul_acl_policy.agent.name]
-  local = true
+  policies    = [consul_acl_policy.agent.name]
+  local       = true
 }
 
 data "consul_acl_token_secret_id" "token" {
-  count = var.workers
-    accessor_id =  element(consul_acl_token.agent-token.*.id, count.index)
+  count       = var.workers
+  accessor_id = element(consul_acl_token.agent-token.*.id, count.index)
 }
 
 resource "random_pet" "animal" {
-  depends_on = [hcp_consul_cluster.hcp_demostack,hcp_vault_cluster.hcp_demostack]
-      length    = 1
-      prefix    = hcp_consul_cluster.hcp_demostack.consul_root_token_secret_id
-      separator = "-"
-    }
+  depends_on = [hcp_consul_cluster.hcp_demostack, hcp_vault_cluster.hcp_demostack]
+  length     = 1
+  prefix     = hcp_consul_cluster.hcp_demostack.consul_root_token_secret_id
+  separator  = "-"
+}
 
 
 resource "consul_service" "vault" {
-  depends_on = [hcp_consul_cluster.hcp_demostack,hcp_vault_cluster.hcp_demostack]
-  name    = "vault"
-  node    = consul_node.vault.name
-  port    = 8200
-  tags    = ["hcp","vault"]
+  depends_on = [hcp_consul_cluster.hcp_demostack, hcp_vault_cluster.hcp_demostack]
+  name       = "vault"
+  node       = consul_node.vault.name
+  port       = 8200
+  tags       = ["hcp", "vault"]
   check {
     check_id                          = "vault_health_check"
     name                              = "hcp vault health check"
@@ -66,8 +66,8 @@ resource "consul_service" "vault" {
 
 
 resource "consul_node" "vault" {
-  depends_on = [hcp_consul_cluster.hcp_demostack,hcp_vault_cluster.hcp_demostack]
-  name    = "compute-vault"
+  depends_on = [hcp_consul_cluster.hcp_demostack, hcp_vault_cluster.hcp_demostack]
+  name       = "compute-vault"
   # address = hcp_vault_cluster.hcp_demostack.vault_public_endpoint_url
- address = replace(hcp_vault_cluster.hcp_demostack.vault_private_endpoint_url, ":8200", "")
+  address = replace(hcp_vault_cluster.hcp_demostack.vault_private_endpoint_url, ":8200", "")
 }
