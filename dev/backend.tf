5aa32cf322beded76e147ef1e8d91fa01999e935
Guy Barros <guy@Hashicorp.com>
2022-11-07 14:33:11 +0000

    remove datasources from dev main.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -8,28 +8,4 @@ terraform {
       name = "GUY-HCP-Demostack-AWS"
     }
   }
-}
-
-// Workspace Data
-data "terraform_remote_state" "tls" {
-  backend = "remote"
-  config = {
-    hostname     = "app.terraform.io"
-    organization = "emea-se-playground-2019"
-    workspaces = {
-      name = "tls-root-certificate"
-    }
-  } //config
-}
-
-data "terraform_remote_state" "dns" {
-  backend = "remote"
-
-  config = {
-    hostname     = "app.terraform.io"
-    organization = "emea-se-playground-2019"
-    workspaces = {
-      name = "Guy-DNS-Zone"
-    }
-  } //network
-}
+  }
