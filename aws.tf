b0e05a329bc44e0a7da400a51ffe3641b9d2b2fe
Guy Barros <guy@Hashicorp.com>
2022-11-16 14:07:24 +0000

    added egress to sg

--- aws.tf
+++ aws.tf
@@ -296,6 +296,13 @@ resource "aws_security_group" "demostack" {
   }
 
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 resource "aws_key_pair" "demostack" {
