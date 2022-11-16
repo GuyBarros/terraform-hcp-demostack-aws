96eaf1de344b7d53444948d8e01c525c4ec01113
Guy Barros <guy@Hashicorp.com>
2022-11-16 12:54:19 +0000

    change the way packages are installed

--- templates/shared/base.sh
+++ templates/shared/base.sh
@@ -48,8 +48,10 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF
 
 echo "--> Adding Hashicorp repo"
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
- sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+
 
 echo "--> updated version of Nodejs"
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
